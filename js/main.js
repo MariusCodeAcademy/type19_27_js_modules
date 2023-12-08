40a9cf974c959f6b4ed38e47348269b5c521f5b2
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-08 08:59:15 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+console.log('main.js file was loaded');
+
+// function skills
+
+// sukurti funkcija kuri suskaiciuoja 4 skaiciu vidurki
+// avg(4,5,8,7) grazina vidurki
+// sukurti funkcija kuri suskaiciuoja masyvo gauto argumento vidurki
+// avgArr([4,1,5,8,7])
+
+// parasyti funkcija kuri pagamina ir grazina html elementa, argumentu paduodam elemento taga, classe, ir texta
+// makeEl('h2', 'bg-primary', 'I am text')
